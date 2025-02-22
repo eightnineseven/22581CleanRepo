0d3635d79c8122cf0e525e21c797ff881b67c657
Jack Scafidi <114788956+eightnineseven@users.noreply.github.com>
2025-02-21 22:43:57 -0500

    checking to see if git worked

--- TeamCode/src/main/java/RedRingOfDeath/Teleop/ExampleTeleop.java
+++ TeamCode/src/main/java/RedRingOfDeath/Teleop/ExampleTeleop.java
@@ -1,4 +1,16 @@
 package RedRingOfDeath.Teleop;
 
-public class ExampleTeleop {
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+public class ExampleTeleop extends OpMode {
+    //all of this gets confusing really fast so we'll worry about it later
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
 }
